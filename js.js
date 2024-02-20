b2e2006509e7ebfc6cca689190bbe618e15a4bd2
Sorakiel <sorakield@gmail.com>
2024-02-20 20:07:55 +0300

    desktop and phone versions v1

---
+++ js.js
@@ -0,0 +1,6 @@
+/*const photos = ["img/Arteom.jpg","img/Nazar.jpg","img/Dasha.jpg","img/Ksy and Kirril.jpg","img/Lia.jpg","img/Ksy and Kirril.jpg"];
+
+elem = document.getElementsByClassName("team_photo");
+elem.style.cssText += photos[i];
+
+ */
